49ab68645b3b3e0e9d8085f869ce68f93b4aa9a5
zint0x <nitish@jungleegames.com>
2019-07-23 17:59:36 +0530

    Change test

---
+++ response/AuthenticationResponse.js
@@ -0,0 +1,8 @@
+// ====================================================================================================
+//
+// Cloud Code for AuthenticationResponse, write your code here to customize the GameSparks platform.
+//
+// For details of the GameSparks Cloud Code API see https://docs.gamesparks.com/
+//
+// ====================================================================================================
+var a = 2;
